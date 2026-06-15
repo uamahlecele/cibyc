0d81a66e8c888cd66ce0ec4d2c42afd5e6e3250c
amceljhb025 <amceljhb025@student.wethinkcode.co.za>
2026-06-15 14:12:35 +0200

    styling and setting up functionality

--- app.js
+++ app.js
@@ -1,31 +1,67 @@
 
 async function establishConnection() {
-    const response = await fetch("http://192.168.1.2:8080/ccapi")
 
-    if (!response.ok) {
-        throw new Error("Could not connect to Camera");
-    } else {
-        console.log("Connection to Camera established! :)")
-    }
+    // const response = await fetch("http://192.168.1.2:8080/ccapi")
+
+    // if (!response.ok) {
+    //     throw new Error("Could not connect to Camera");
+    // } else {
+    //     console.log("Connection to Camera established! :)")
+    // }
+
+    const response = await fetch("http://localhost:8000/connect")
 }
+
+// async function cameraLiveView() {
+
+//     // This will call my end point to my server.js to make the request
+
+//     const liveView = await fetch("http://localhost:8000/liveview");
+
+
+// }
+
+
+
+
+
+
+
+
 async function cameraLiveView() {
 
-    const dataLi = await fetch("http://192.168.1.2:8080/ccapi/ver100/shooting/liveview", {
+    const dataLiveView = await fetch("http://192.168.1.2:8080/ccapi/ver100/shooting/liveview", {
         method: 'POST', body: {
             'liveviewsize': 'small',
-            'cameradisplay': 'small'
+            'cameradisplay': 'on'
         }
     })
 
-    const imageTag = document.getElementById('liveView');
-
-    const data = await fetch("http://192.168.1.2:8080/ccapi/ver100/shooting/liveview/flip")
-
     const response = await data.json()
+    console.log("This is the initial API call and it should display only Content Type json/ application if it is successful! ", dataLiveView.json());
 
-    console.log("THIS IS WHAT THE DATA LOOKS LIKE: " + response)
 
-    imageTag.src = data
+    if (dataLiveView.ok) {
+
+        const actualImageFromCamera = document.getElementById('displayImageFromCamera');
+
+        const dataImageFromLiveView = await fetch("http://192.168.1.2:8080/ccapi/ver100/shooting/liveview/flip")
+        actualImageFromCamera.src = dataImageFromLiveView;
+
+    }
+    else {
+        console.log("liveview call failed!");
+    }
+
+    // const imageTag = document.getElementById('liveView');
+
+
+
+    // const response = await data.json()
+
+    // console.log("THIS IS WHAT THE DATA LOOKS LIKE: " + response)
+
+
 }
 
 // const result = document.getElementById('testingAPI').textContent = data
--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 const express = require('express')
+const http = require('http')
+
 const app = express()
 // const cors = require('cors')
 const liveView = require('./app.js');
 
 // app.use(cors())
 
+// http
+//     .post('http://192.168.1.2:8080/ccapi/ver100/shooting/liveview', {
+//         method: 'POST', body: {
+//             'liveviewsize': 'small',
+//             'cameradisplay': 'on'
+//         }
+//     })
+
 app.listen(8000, () => {
     console.log('Running on port 8000')
 })
 
 
-app.get('/index', (req, res) => { })
+app.get('/connect', async (req, res) => {
+    const response = await fetch("http://192.168.1.2:8080/ccapi")
+
+    if (!response.ok) {
+        throw new Error("Could not connect to Camera");
+    } else {
+        // console.log("Connection to Camera established! :)")
+        res.send("Connection to Camera established! :)")
+    }
+
+})
+
+app.post('/liveview', async (req, res) => {
+    await fetch('http://192.168.1.2:8080/ccapi/ver100/shooting/liveview', {
+        method: 'POST', body: {
+            'liveviewsize': 'small',
+            'cameradisplay': 'on'
+        }
+    })
+
+})
